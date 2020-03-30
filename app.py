33b2705bfdb52c6ea19ef2dee9570ee7e6397571
Arijith <arijith96@gmail.com>
2020-03-30 18:06:33 +0200

    Factorial after Addition Feature

--- app.py
+++ app.py
@@ -6,10 +6,20 @@ def add(a,b):
 def sub(a,b):
     return a-b
 
+def add_fact(a,b):
+	return fact(add(a,b))
 
+def fact(a):
+	if a < 0: 
+		return "Factorial of Negative Number!"
+	if a == 0 or a==1:
+		return 1
+	else:
+		return a * fact(a-1)
 
 if __name__=='__main__':
     a = int(sys.argv[1])
     b = int(sys.argv[2])
     print(add(a,b))
     print(sub(a,b))
+    print(add_fact(a,b))
