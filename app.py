5e3c2669ed7b089cb932f77b7b27fc3df904ad66
Chaithra Mahadeva <61188812+ChaithraMahadeva@users.noreply.github.com>
2020-03-30 17:43:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,17 @@ def add(a,b):
 def sub(a,b):
     return a-b
 
+def facofsub(a,b)
+    if(a>b) diff=a-b
+    else diff=b-a
+    return factorial(diff)
+
+def factorial(c)
+    if(c==0)
+    return 1
+    return c*factorial(c-1)
+    
+
 
 
 if __name__=='__main__':
@@ -13,3 +24,4 @@ if __name__=='__main__':
     b = int(sys.argv[2])
     print(add(a,b))
     print(sub(a,b))
+    print(facofsub(a,b))
